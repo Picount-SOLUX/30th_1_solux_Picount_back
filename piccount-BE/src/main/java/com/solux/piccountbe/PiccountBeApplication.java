13dbc2ea57d252f758db046ffb0607c245dc375d
sujeongmoon <msjk9000@gmail.com>
2025-07-07 02:25:23 +0900

    feat: #1 - 프로그램 초기 세팅 완료

---
+++ piccount-BE/src/main/java/com/solux/piccountbe/PiccountBeApplication.java
@@ -0,0 +1,13 @@
+package com.solux.piccountbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiccountBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiccountBeApplication.class, args);
+	}
+
+}
