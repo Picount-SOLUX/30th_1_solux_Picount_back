f6d18fcab959ee9ec02fb0d99622ee09c91c8a92
sujeongmoon <msjk9000@gmail.com>
2025-07-07 15:03:53 +0900

    feat: #4 - Friend, GuestBook Entity 추가

---
+++ piccount-BE/src/main/java/com/solux/piccountbe/domain/friend/entity/Friend.java
@@ -0,0 +1,39 @@
+package com.solux.piccountbe.domain.friend.entity;
+
+import com.solux.piccountbe.domain.member.entity.Member;
+import com.solux.piccountbe.global.Timestamped;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Friend extends Timestamped {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Long friendId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "friend_member_id", nullable = false)
+	private Member friendMember;
+
+	@Column(nullable = false)
+	@Enumerated(value = EnumType.STRING)
+	private Status status;
+}
---
+++ piccount-BE/src/main/java/com/solux/piccountbe/domain/friend/entity/Status.java
@@ -0,0 +1,15 @@
+package com.solux.piccountbe.domain.friend.entity;
+
+public enum Status {
+	APPROVAL;
+
+	// private final String label;
+	//
+	// Type(String label) {
+	// 	this.label = label;
+	// }
+	//
+	// public String getLabel() {
+	// 	return label;
+	// }
+}
