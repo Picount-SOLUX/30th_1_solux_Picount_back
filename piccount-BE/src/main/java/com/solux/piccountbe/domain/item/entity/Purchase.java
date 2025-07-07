942d706274c54fbd72d4be43d379c17216d01bdf
sujeongmoon <msjk9000@gmail.com>
2025-07-07 14:29:09 +0900

    feat: #4 - Item, Purchase, Sticker Entity 추가

---
+++ piccount-BE/src/main/java/com/solux/piccountbe/domain/item/entity/Purchase.java
@@ -0,0 +1,37 @@
+package com.solux.piccountbe.domain.item.entity;
+
+import java.time.LocalDateTime;
+
+import com.solux.piccountbe.domain.member.entity.Member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Purchase {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Long purchaseId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "item_id", nullable = false)
+	private Item item;
+
+	@Column(nullable = false)
+	private LocalDateTime purchasedAt;
+}
---
+++ piccount-BE/src/main/java/com/solux/piccountbe/domain/item/entity/Sticker.java
@@ -0,0 +1,29 @@
+package com.solux.piccountbe.domain.item.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Sticker {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Long stickerId;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "item_id", nullable = false)
+	private Item item;
+
+	@Column(nullable = false)
+	private String stickerUrl;
+}
