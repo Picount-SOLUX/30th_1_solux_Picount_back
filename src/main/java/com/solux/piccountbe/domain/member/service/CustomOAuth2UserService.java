9f86c75c93a0bfcfea0f930927af17c01efe2e03
sujeongmoon <msjk9000@gmail.com>
2025-07-14 16:27:32 +0900

    comment: #20 - 주석 변경

--- src/main/java/com/solux/piccountbe/domain/member/service/CustomOAuth2UserService.java
+++ src/main/java/com/solux/piccountbe/domain/member/service/CustomOAuth2UserService.java
@@ -37,8 +37,8 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 		Map<String, Object> account = (Map<String, Object>)attributes.get("kakao_account");
 		Map<String, Object> profile = (Map<String, Object>)account.get("profile");
 
-		log.info("kakao_acount: {}", account);
-		log.info("profile: {}", profile);
+		// log.info("kakao_acount: {}", account);
+		// log.info("profile: {}", profile);
 
 		if (account == null || profile == null) {
 			return null;
@@ -50,6 +50,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 		// 이메일
 		String kakaoEmail = (String)account.get("email");
 
+		// TODO: 프론트와 연동 후 범위 설정
 		Member member = memberRepository.findByProviderAndOauthId(Provider.KAKAO, kakaoId)
 			.map(m -> m.memberUpdate(kakaoEmail))
 			.orElseGet(() -> memberRepository.save(
@@ -67,52 +68,6 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
 		return UserDetailsImpl.create(member, attributes);
 
-		// public
-		// Member(Provider provider, String email, String password, String nickname, String profileImageUrl, Gender gender, String friendCode, Integer age, Boolean withdraw, Boolean isMainVisible) {
-		// 	this.provider = provider;
-		// 	this.email = email;
-		// 	this.password = password;
-		// 	this.nickname = nickname;
-		// 	this.profileImageUrl = profileImageUrl;
-		// 	this.gender = gender;
-		// 	this.friendCode = friendCode;
-		// 	this.age = age;
-		// 	this.withdraw = withdraw;
-		// 	this.isMainVisible = isMainVisible;
-		// 	));
-
 	}
 
 }
-
-// 이메일 꺼내기
-
-// // 프로바이더 구분
-// String registrationId = oAuth2UserRequest.getClientRegistration().getRegistrationId();
-//
-// // 프로바이더별 userINfo 파싱
-// OAuth2UserInfo userInfo = oAuth2UserInfoFactory.getOAuth2UserInfo(
-// 	registrationId, oAuth2User.getAttributes());
-//
-// //DB에 이미 있으면 업데이트, 없으면 회원ㄱ아ㅣㅂ
-// Member member = registerOrUpdateMember(registrationId, userInfo);
-//
-// //시큐리티 컨텍스트에 담을  User 객체 반환
-// return UserPrincipal.create(member, oAuth2User.getAttributes());
-//}
-
-// private Member registerOrUpdateMember(String registrationId, OAuth2UserInfo userInfo) {
-// 	return memberRepository.findByProviderAndProviderId(registrationId, userInfo.getId())
-// 		.map(existing -> existing.update(userInfo.getName(), userInfo.getEmail()))
-// 		.orElseGet(() -> memberRepository.save(
-// 			Member.builder()
-// 				.provider(registrationId)
-// 				.providerId(userInfo.getId())
-// 				.email(userInfo.getEmail())
-// 				.name(userInfo.getName())
-// 				// .role(Role.USER)
-// 				.build()
-// 		));
-//
-// }
-
