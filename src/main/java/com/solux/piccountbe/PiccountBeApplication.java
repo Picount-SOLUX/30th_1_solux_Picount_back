570a6641c5a8363a236d0bce8c64470c10bb80f2
semsemin <semin0409@naver.com>
2025-07-23 02:12:17 +0900

    배포 설정 (dockerfile, docker-compose, rds 연결)

--- src/main/java/com/solux/piccountbe/PiccountBeApplication.java
+++ src/main/java/com/solux/piccountbe/PiccountBeApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableScheduling
 public class PiccountBeApplication {
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(PiccountBeApplication.class, args);
 	}
 
